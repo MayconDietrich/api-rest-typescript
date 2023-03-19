58d426683319ba3dbdbc58f7beeea58c796677e1
Maycon Dietrich <maycon.dietrich@gmail.com>
2023-03-19 16:20:26 -0300

    listagem de registros

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { roomRepository } from "../repositories/roomRepository";
+import { subjectRepository } from "../repositories/subjectRepository";
 import { videoRepository } from "../repositories/videoRepository";
 
 export class RoomController {
   async create(req: Request, res: Response) {
     const { name, description } = req.body;
-    //iup
 
     try {
       const newRoom = roomRepository.create({ name, description });
@@ -34,6 +34,57 @@ export class RoomController {
       await videoRepository.save(newVideo);
 
       return res.status(201).json(newVideo);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: 'internal server error'
+      })
+    }
+  }
+
+  async roomSubject(req: Request, res: Response) {
+    const { subject_id } = req.body;
+    const { roomId } = req.params;
+
+
+    try {
+      const room = await roomRepository.findOneBy({ id: Number(roomId) });
+      if (!room) {
+        return res.status(404).json({
+          message: 'Aula não encontrada'
+        })
+      }
+      const subject = await subjectRepository.findOneBy({ id: Number(subject_id) });
+
+      if (!subject) {
+        return res.status(404).json({
+          message: 'Aula não encontrada'
+        })
+      }
+      const roomUpdate = {
+        ...room,
+        subjects: [subject]
+      }
+      await roomRepository.save(roomUpdate);
+
+      return res.status(204).send();
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: 'internal server error'
+      })
+    }
+  }
+
+  async list(req: Request, res: Response) {
+    try {
+      const rooms = await roomRepository.find({
+        relations: {
+          subjects: true,
+          videos: true
+        }
+      });
+      return res.json(rooms);
     } catch (err) {
       console.log(err);
       return res.status(500).json({
